2f0033cba4c4e815815d6e57b3e663db06a8ef75
卷心菜汪 <Rv_Jiang@outlook.com>
2022-04-28 17:22:11 +0800

    feat: vuex自动导入modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,22 @@
  * @Author: Rv_Jiang
  * @Date: 2022-04-27 18:09:50
  * @LastEditors: Rv_Jiang
- * @LastEditTime: 2022-04-28 16:30:54
+ * @LastEditTime: 2022-04-28 17:16:05
  * @Description: Vuex配置文件
  * @Email: Rv_Jiang@outlook.com
  */
 import { createStore } from 'vuex';
 
-const store = createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {},
+// 动态获取Store模块
+const modulesFiles = import.meta.globEager('./modules/*.js');
+const modules: any = {};
+Object.keys(modulesFiles).forEach(item => {
+  const moduleName = item.replace(/^\.\/modules\/(.*)\.\w+$/, '$1');
+  modules[moduleName] = modulesFiles[item].default;
 });
+console.log(modules);
 
-export default store;
+// 创建一个新的 store 实例
+export default createStore({
+  modules,
+});
