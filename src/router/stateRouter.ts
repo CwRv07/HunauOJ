bdb36b7d1ab8d1e3997c0e5efcfe1a5a44f50014
卷心菜汪 <Rv_Jiang@outlook.com>
2022-07-03 11:17:49 +0800

    feat(echarts+olpbarchart): echarts引入和长条图echarts封装

--- src/router/stateRouter.ts
+++ src/router/stateRouter.ts
@@ -2,7 +2,7 @@
  * @Author: Rv_Jiang
  * @Date: 2022-05-01 18:56:23
  * @LastEditors: Rv_Jiang
- * @LastEditTime: 2022-07-03 09:53:20
+ * @LastEditTime: 2022-07-03 11:09:52
  * @Description: 公开路由表
  * @Email: Rv_Jiang@outlook.com
  */
@@ -63,6 +63,10 @@ const stateRouter: RouteRecordRaw[] = [
   {
     path: '/markdownTest',
     component: () => import('@/views/markdownTest/markdownTest.vue'),
+  },
+  {
+    path: '/test',
+    component: () => import('@/views/test/test.vue'),
   },
 ];
 
---
+++ src/views/test/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="echartsContainer">
+      <olp-bar-chart />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.echartsContainer {
+  width: 100%;
+  height: 500px;
+}
+</style>
